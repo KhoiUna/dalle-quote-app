c37d059308731de0d573f15f5345131bd4be3e36
KhoiUna <thorwaitson@gmail.com>
2023-04-03 23:09:37 -0500

    modified:   app/components/Loader.tsx

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -2,7 +2,7 @@ export default function Loader() {
     return (
         <>
             {/* @ts-ignore */}
-            <lottie-player src="https://assets6.lottiefiles.com/packages/lf20_JvPPeJ.json" background="transparent" speed="1" style={{ width: '300px', height: '300px' }} loop autoplay />
+            <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_Po6f5j.json" background="transparent" speed="1" style={{ width: '300px', height: '300px' }} loop autoplay />
         </>
     )
 }
